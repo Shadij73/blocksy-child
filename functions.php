07f0b5016b6efb968b3b78ac0618a54b96c70c25
Shadij73 <shadij73@hotmail.com>
2024-08-14 21:39:42 +0200

    Ready01

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 add_filter('wp_nav_menu_items', 'hide_admin_menu_item', 10, 2);
-//Test comment01
+
 function hide_admin_menu_item($items, $args) {
     if (!is_user_logged_in()) {
         if (stripos($items, 'Admin') !== false) {
